536a9a7eab01925f068c113847bb657d65aa8310
carlosmsx <carlosmsx@gmail.com>
2022-06-07 23:39:24 -0300

    agregado ejercicio de objetos 7 (estructura base)

---
+++ js/tp3-objetos-07.js
@@ -0,0 +1,102 @@
+/*
+7- Nos piden realizar una agenda telefónica de contactos.
+
+Un contacto está definido por un nombre y un teléfono. Se considera que un contacto es igual a otro cuando sus nombres son iguales.
+
+Una agenda de contactos está formada por un conjunto de contactos. Se podrá crear de dos formas, indicando nosotros el tamaño o con un tamaño por defecto (10).
+
+Los métodos de la agenda serán los siguientes:
+
+aniadirContacto(Contacto): Añade un contacto a la agenda, sino la agenda no puede almacenar mas contactos indicar por pantalla.
+existeContacto(Conctacto): indica si el contacto pasado existe o no.
+listarContactos(): Lista toda la agenda
+buscarContacto(nombre): busca un contacto por su nombre y muestra su teléfono.
+eliminarContacto(Contacto c): elimina el contacto de la agenda, indica si se ha eliminado o no por pantalla
+agendaLlena(): indica si la agenda está llena.
+huecosLibres(): indica cuántos contactos más podemos ingresar.
+
+Crea un menú con opciones por consola para probar todas estas funcionalidades.
+*/
+
+class Contacto 
+{
+    constructor(nombre, telefono)
+    {
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+
+    get getNombre() { return this.nombre; }
+    get getTelefono() { return this.telefono; }
+}
+
+class Agenda
+{
+    constructor(tamanio)
+    {
+        this.tamanio = tamanio;
+        this.contactos = [];
+    }
+
+    //Añade un contacto a la agenda, sino la agenda no puede almacenar mas contactos indicar por pantalla.
+    aniadirContacto(contacto) 
+    {
+        if (this.agendaLlena() == false)
+        {
+            this.contactos.push(contacto);
+        }
+        else
+        {
+            alert("No se pueden añadir mas contactos.");
+        }
+    }
+    
+    //indica si el contacto pasado existe o no.
+    existeContacto(contacto)
+    {
+        return this.buscarContacto(contacto.nombre) != undefined;
+    }
+
+    //Lista toda la agenda
+    listarContactos()
+    {
+        this.contactos.forEach(contacto => {
+            document.write(`nombre: ${contacto.getNombre} (${contacto.getTelefono})<br>`);
+        })
+    }
+
+    //busca un contacto por su nombre y muestra su teléfono.
+    buscarContacto(nombre)
+    {
+        return this.contactos.find(contacto => contacto.getNombre == nombre)
+    }
+
+    //elimina el contacto de la agenda, indica si se ha eliminado o no por pantalla
+    eliminarContacto(contacto)
+    {
+
+    }
+
+    //indica si la agenda está llena.
+    agendaLlena()
+    {
+        return this.tamanio == this.contactos.length;
+    }
+
+    //indica cuántos contactos más podemos ingresar.
+    huecosLibres()
+    {
+        return this.tamanio - this.contactos.length;
+    }
+}
+
+let agenda = new Agenda(10);
+
+agenda.aniadirContacto(new Contacto("carlos", "123456"));
+agenda.aniadirContacto(new Contacto("Alejandro", "3232323"));
+agenda.aniadirContacto(new Contacto("fede", "442341"));
+
+agenda.listarContactos();
+
+let contacto = agenda.buscarContacto("Alejandro");
+console.log(contacto);
